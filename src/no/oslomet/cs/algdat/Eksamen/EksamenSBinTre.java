6ba681b22d4f75c9406b58d5bccb060bfde9bf56
Erik Myhre <myherik@gmail.com>
2020-10-21 23:27:10 +0200

    begynner på oppg. 3
    Fjernet UnsupportedOperationException til oppg. 3

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -159,7 +159,7 @@ public class EksamenSBinTre<T> {
     }
 
     private static <T> Node<T> førstePostorden(Node<T> p) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        
     }
 
     private static <T> Node<T> nestePostorden(Node<T> p) {
